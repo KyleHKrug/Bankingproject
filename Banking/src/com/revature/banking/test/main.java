08d75f8220d580a63790e66ed0d924f188aea08c
KyleHKrug <59713832+KyleHKrug@users.noreply.github.com>
2020-01-24 09:43:21 -0500

    Add files via upload

---
+++ Banking/src/com/revature/banking/test/main.java
@@ -0,0 +1,16 @@
+package com.revature.banking.test;
+
+
+
+public class main {
+	public static void main(String [] args) {
+		//example of how to write to the specific file
+//		Customer customer = new Customer();
+//		ObjectIO wObj = new ObjectIO();
+//		customer.NewAccount();
+//		wObj.writeObject(customer,customer.getFileloc()+customer.getId() );
+		
+
+	}	
+}
+
