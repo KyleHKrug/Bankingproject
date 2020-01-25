a0514f35d5f09724e927d86d7703ee5eec7dc9c9
Alia Reed <alia@fortreed.com>
2020-01-25 18:07:39 -0500

    kyle help

--- Banking/src/com/revature/banking/test/Main.java
+++ Banking/src/com/revature/banking/test/Main.java
@@ -2,20 +2,24 @@ package com.revature.banking.test;
 
 public class Main {
 	public static void main(String [] args) {
+		
 //		example of how to write to the specific file
+
+		
+		//Admin admin = new Admin();
+		//admin.getCustomer();
+		
+		
 		Customer customer = new Customer();
-		ObjectIO rObj = new ObjectIO();
+		ObjectIO wObj = new ObjectIO();
 		customer.NewAccount();
+		wObj.writeObject(customer, customer.getFileloc() + customer.getId());
+		ObjectIO rObj = new ObjectIO();
 		rObj.readObject(customer, customer.getFileloc() + customer.getId());
-		//customer = (Customer)readObject;
 		
 		//ObjectIO object = new ObjectIO();
 		//object.readObject(object, "00001");
-		
-		
-		//Customer customer = new Customer();
-		
-
+			
 		//customer.loginCustomer();
 		
 		//Employee employee = new Employee();
