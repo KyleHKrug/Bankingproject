1b2dccb59480e2b19aa761fcb53e0ab2346aafe5
Alia Reed <alia@fortreed.com>
2020-01-26 03:51:25 -0500

    it works now except for employee

--- Banking/src/com/revature/banking/test/Employee.java
+++ Banking/src/com/revature/banking/test/Employee.java
@@ -5,138 +5,148 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Employee extends Account {
-	
+
 	// used to display the balance to the second decimal place
 	private static DecimalFormat df = new DecimalFormat("0.00");
 	private static final long serialVersionUID = 1L;
 	private String fileloc = Paths.get("").toAbsolutePath().toString();
 
 	public void employeeLogin() {
-		
+
 		Employee employee = new Employee();
 		try { 
-			
+
 			Scanner in = new Scanner(System.in);
 			String id = null;
-			boolean t= false;
-			
-			while( t == false) {
-			System.out.println("Enter your ID number: ");
+			boolean t = false;
+
+			while(t == false) {
+				
+			System.out.println("Please enter your ID number!");
 			id = in.nextLine();
-			t= employee.checkNull(id);
-			}
+			t = employee.checkNull(id);
 			
-			String pass= null;
+			}
+
+			String pass = null;
 			boolean w = false;
-			
+
 			while(w == false) {
-				System.out.println("Enter your password: ");
+				
+				System.out.println("Please enter your password!");
 				pass = in.nextLine();
-				w= employee.checkNull(pass);
+				w = employee.checkNull(pass);
+				
 			}
-			
+
 			Login check = new Login();
 			ObjectIO robj = new ObjectIO();
-			
-			employee = (Employee) robj.readObject(employee,employee.getFileloc()+"\\Employee\\"+ id);
+
+			employee = (Employee) robj.readObject(employee, "/Users/aliareed/Bankingproject/Banking/Employee/" + id);
 			String password = employee.getPassword();
-			
+
 					if(check.login(pass, password) == true) {
-						
-						System.out.println("Logged in");
-						in.close();
-						
+
+						System.out.println("Logged in!");
+					//	in.close();
+
 					}else{
-						
-						System.out.println("Failed to log in, try again");
+
+						System.out.println("Cannot log in, try again!");
 						employeeLogin();
 						in.close();
-						
+
 						}
 			in.close();
-			
+
 		}catch (Exception ex) {
-			
+
 			System.out.println("Account does not exist");
 			employeeLogin();
-		}
-		
-	}
-	
-	public void employeeMenu() {
-		
-		Customer customer = new Customer();
-		
-		try (Scanner in = new Scanner(System.in)) {
 			
-			System.out.println("###########################################################################");
+		}
+
+	}
+
+	public void employeeMenu() {
+
+		Customer customer = new Customer();
+
+		try (Scanner in = new Scanner(System.in)) {
+
 			System.out.println("Enter Customer ID to view their information");
 			String id = in.nextLine();
 			ObjectIO objI = new ObjectIO();
-			customer = (Customer)objI.readObject(customer, id);
+			customer = (Customer)objI.readObject(customer, "/Users/aliareed/Bankingproject/Banking/Customer/" + id);
 			String c ="";
-			
+
 			while(c != "e") {
-				
+
 				System.out.println("Enter (V) to view all customer information");
-				System.out.println("Enter (A) to Approve the customer account ");
+				System.out.println("Enter (A) to Approve the customer account");
 				System.out.println("Enter (E) to Exit ");
 				c =in.nextLine().toLowerCase();
-				
+
 				if(c.equalsIgnoreCase("v")) {
-					
+
 					view_customer(customer);
-					
+
 				}else if(c.equalsIgnoreCase("a")) {
 					
+					approve(customer);
+
 				}else if(c.equalsIgnoreCase("e")) {
-					
+
 					c = "e";
-				}else {
+					System.out.println("Thanks for coming! Have a good day!");
+					System.exit(0);
 					
-					System.out.println("Not a valid command");
+				}else {
+
+					System.out.println("Try again buster");
 				}
 			}
-			
+
 			in.close();
 		}
 	}
-	
+
 	public void approve(Customer customer) {
-		
+
 		Scanner in = new Scanner(System.in);
-		
+
 		if(customer.isApproved()== false) {
-			
+
 			System.out.println("Do you wish to approve the this client? [Y/N]");
 			String c = in.nextLine();
-			
+
 			if(c.equalsIgnoreCase("y")) {
-				
+
 				System.out.println("Approved");
 				customer.setApproved(true);
-				
+
 				ObjectIO wobj = new ObjectIO();
-				wobj.writeObject(customer, customer.getFileloc() + customer.getId());
-				
+				wobj.writeObject(customer, "/Users/aliareed/Bankingproject/Banking/Customer/" + customer.getId());
+
 			}else {
-				
-				System.out.println("Enter anything to load another customer");
+
+				System.out.println("Enter anything to load another customer!");
 				String d = in.nextLine();
-				
+
 				while(d == null) {
+					
 					d = in.nextLine();
+					
 				}
-				
+
 				in.close();				
 			}
 		}
 	}
-	
+
 	public void view_customer(Customer customer) {
 		// used to see customer information
-		
-		System.out.println("###########################################################################");
+
 		System.out.println("Account holder: " + customer.getLast_name() + "," + customer.getFirst_name());
 		System.out.println("Holder id : " +customer.getId());
 		System.out.println("Holder address : " +customer.getAddress());
@@ -144,12 +154,12 @@ public class Employee extends Account {
 		System.out.println("Holder additional info : " + customer.getInfo());
 		System.out.println("Approved: " + customer.isApproved());
 		}
-	
+
 	public String getFileloc() {
 		return fileloc;
 	}
-	
+
 	public void setFileloc(String fileloc) {
 		this.fileloc = fileloc;
 	}	
-}
+} 
--- Banking/src/com/revature/banking/test/Main.java
+++ Banking/src/com/revature/banking/test/Main.java
@@ -1,40 +1,126 @@
 package com.revature.banking.test;
 
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String [] args) {
-		
-//		example of how to write to the specific file
-
-		
-		//Admin admin = new Admin();
-		//admin.getCustomer();
-		
-
-		Customer customer = new Customer();
-		ObjectIO rObj = new ObjectIO();
-		customer.NewAccount();
-		rObj.writeObject(customer, customer.getFileloc()+customer.getId());
-		customer = (Customer) rObj.readObject(customer, customer.getFileloc() + "00001");
-		System.out.println(customer.getFirst_name());
-		
-		
-//		Customer customer = new Customer();
-//		ObjectIO wObj = new ObjectIO();
-//		customer.NewAccount();
-//		wObj.writeObject(customer, customer.getFileloc() + customer.getId());
-//		ObjectIO rObj = new ObjectIO();
-//		rObj.readObject(customer, customer.getFileloc() + customer.getId());
-		
-		//ObjectIO object = new ObjectIO();
-		//object.readObject(object, "00001");
-			
-		//customer.loginCustomer();
-		//Employee employee = new Employee();
-		//employee.employeeMenu();
 	
-		//Admin admin = new Admin();
+	@SuppressWarnings({ "resource", "unused" })
+	public static void main(String [] args) {
+			
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		Admin admin = new Admin();
+		ObjectIO rObj = new ObjectIO();
+		ObjectIO wObj = new ObjectIO();
+
+		Scanner scanningStan = new Scanner(System.in);
+		String userType;
+		
+		System.out.println("Hello and welcome to Kyle and Alia's Bank!\n\nAre you a:\n");
+		System.out.println("1. New customer?");
+		System.out.println("2. Returning customer?");
+		System.out.println("3. Employee?");
+		System.out.println("4. Administrator?");
+		
+		userType = scanningStan.next();
+		
+		switch(userType) {
+		
+			case "1":
+
+				String choices;
+				System.out.println("\nWelcome valued new customer!");
+				System.out.println("Would you like to:");
+				System.out.println("1. Create an account?");
+				System.out.println("2. Go away?");
+				choices = scanningStan.next();
+				
+				if(choices.equals("1")) {
+										
+					customer.NewAccount();
+
+					wObj.writeObject(customer, "/Users/aliareed/Bankingproject/Banking/Customer/" + customer.getId());
+					
+					System.out.println("Your account application is being processed! We'll get back to you shortly! Have a nice day!");
+
+				}else {
+					
+					System.out.println("\nBye!");
+					System.exit(0);
+					
+				}
+				
+				break;
+				
+			case "2":
+				
+				System.out.println("\nWelcome valued returning customer!");
+			
+				customer.loginCustomer();
+				customer.menu(customer);
+				
+				break;
+				
+			case "3":
+				
+				employee.employeeLogin();
+				employee.employeeMenu();
+//				employee.approve(customer);
+				
+//				System.out.println("\nWelcome valued employee!");
+//				System.out.println("What would you like to do today?");
+//				
+//				System.out.println("Please enter the user ID of the customer you wish to approve/deny!");
+//				choices = scanningStan.next();
+//				
+//				customer = (Customer)rObj.readObject(customer, "/Users/aliareed/Bankingproject/Banking/Customer/" + choices);
+//				
+//				employee.approve(customer);
+//																
+//				wObj.writeObject(customer, "/Users/aliareed/Bankingproject/Banking/Customer/aa");
+//				customer.isApproved();
+
+				
+				break;
+				
+			case "4":
+				
+				System.out.println("\nWelcome valued administrator!");
+//				
+//				admin.createEmployee();
+//				
+//				wObj1.writeObject(employee1, "/Users/aliareed/Bankingproject/Banking/Employee/");
+//				
+//				System.out.println("Welcome to the family, new employee!");
+				
+				break;
+				
+			default:
+				
+				break;
+		}			
+//		scanningStan.close();
+	}
+		
+//	@SuppressWarnings("resource")
+//	public static boolean mariosMenu() {
+//	
+//		return true;
+				
+		//customer.loginCustomer();
+		//customer.newAccount();
+	//	rObj.writeObject(customer, customer.getFileloc()+customer.getId());
+	//	customer = (Customer) rObj.readObject(customer, customer.getFileloc() + "a");
+	//	System.out.println(customer.getFirst_name());
+		//customer = (Customer)readObject;
+
+		//object.readObject(object, "00001");
+
+		//customer.loginCustomer();
+
+		//employee.view_customer(customer);
+
 		//admin.adminLogin();
-
-	}	
+//	}
+		
 }
-
